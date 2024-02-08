9a7151fa1069a1df2d01b6766906c4140305dd63
Amedeo Zitti <amedeo.zitti@gmail.com>
2024-02-08 12:54:37 +0100

    feat: now skips corrupted files, accept old generated files with 'since' declared without $date

--- generate_csv_time_series.js
+++ generate_csv_time_series.js
@@ -5,86 +5,114 @@ const fs = require('fs');
 
 var averageReplicaIndex = false;
 const mode = process.argv[2];
-const outFileName = process.argv[3];	
+const outFileName = process.argv[3];
 
-if(mode == 'average'){
-	averageReplicaIndex = true;
+if (mode == 'average') {
+    averageReplicaIndex = true;
 }
 
 var maxNOfReplicas = 0;
 var data = {};
 fs.readdirSync(folder).forEach(file => {
-	if(file.startsWith("index_stats")){
-  		var json = JSON.parse(fs.readFileSync(file));
+    if (file.startsWith("index_stats")) {
+        try {
+            var json = JSON.parse(fs.readFileSync(file));
+        } catch (e) {
+            console.log(`\nError parsing file ${file} \n`)
+            console.log(e);
+            console.log(`\nFile SKIPPED \n`)
+            return;
+        }
 
-  		var extractionDate = json.extractionDate;
-		data[extractionDate] = {};
-		var nOfReplicas = json.replicasInfo.length;
-		if(nOfReplicas > maxNOfReplicas ){
-			maxNOfReplicas = nOfReplicas;
-		}
-  		json.replicasInfo.map(replica => {	
-			var replicaName = Object.keys(replica).sort()[0];
-			replica[replicaName].map(index => {
+        var extractionDate = json.extractionDate;
+        data[extractionDate] = {};
+        var nOfReplicas = json.replicasInfo.length;
+        if (nOfReplicas > maxNOfReplicas) {
+            maxNOfReplicas = nOfReplicas;
+        }
+        json.replicasInfo.map(replica => {
+            var replicaName = Object.keys(replica).sort()[0];
+            replica[replicaName].map(index => {
 
-				if(averageReplicaIndex){
-					var value = data[extractionDate][`${index.name}`];
-					if(value == null){
-						var ops = index.accesses.ops;
-						var since = new Date(`${index.accesses.since.$date}`);
-						var mins = (extractionDate-since.getTime())/1000/60;//per min
-						var timesPerMin = (ops/mins);
-						data[extractionDate][`${index.name}`] = timesPerMin;
-					}else{
-						var ops = index.accesses.ops;
-						var since = new Date(`${index.accesses.since.$date}`);
-						var mins = (extractionDate-since.getTime())/1000/60;//per min
-						var timesPerMin = (ops/mins);
-						value = (value + timesPerMin);
-						data[extractionDate][`${index.name}`] = value;
-					}
-				}else{
-					var ops = index.accesses.ops;
-					var since = new Date(`${index.accesses.since.$date}`);
-					var mins = (extractionDate-since.getTime())/1000/60;//per min
-					var timesPerMin = (ops/mins);
-					data[extractionDate][`${replicaName}_${index.name}`] = timesPerMin;
-				}
-			})
-  		});
-  		
-	}
+                if (averageReplicaIndex) {
+                    var value = data[extractionDate][`${index.name}`];
+                    if (value == null) {
+                        var ops = index.accesses.ops;
+                        var since = getSince(index);
+                        if (index.accesses.since.$date == null) {
+                            since = new Date(`${index.accesses.since.$date}`);
+                        } else {
+                            //old versions generates without
+                            since = new Date(`${index.accesses.since}`);
+                        }
+                        var mins = (extractionDate - since.getTime()) / 1000 / 60; //per min
+                        var timesPerMin = (ops / mins);
+                        data[extractionDate][`${index.name}`] = timesPerMin;
+                    } else {
+                        var ops = index.accesses.ops;
+                        var since = getSince(index);
+                        var mins = (extractionDate - since.getTime()) / 1000 / 60; //per min
+                        var timesPerMin = (ops / mins);
+                        value = (value + timesPerMin);
+                        data[extractionDate][`${index.name}`] = value;
+                    }
+                } else {
+                    var ops = index.accesses.ops;
+                    var since = getSince(index);
+                    var mins = (extractionDate - since.getTime()) / 1000 / 60; //per min
+                    var timesPerMin = (ops / mins);
+                    data[extractionDate][`${replicaName}_${index.name}`] = timesPerMin;
+                }
+            })
+        });
+
+    }
 });
 
 //Define csv header
 var header = "date,";
-Object.keys(data[Object.keys(data).sort()[0]]).sort().forEach(replicaIndex => header=header+replicaIndex+",")
+Object.keys(data[Object.keys(data).sort()[0]]).sort().forEach(replicaIndex => header = header + replicaIndex + ",")
 
 
-if(averageReplicaIndex){
-	console.log(`Results is the average of the ${maxNOfReplicas} replicas`);	
-}else{
-	console.log(`Results are unique foreach replica`);	
+if (averageReplicaIndex) {
+    console.log(`Results is the average of the ${maxNOfReplicas} replicas`);
+} else {
+    console.log(`Results are unique foreach replica`);
 }
 
 console.log(`Writing output to ${outFileName}`);
 
 var fileWriter = fs.createWriteStream(outFileName, {
-  flags: 'w'
+    flags: 'w'
 })
 
 fileWriter.write(`${header}\n`);
 Object.keys(data).forEach(extractionDate => {
-	var d = new Date(parseInt(extractionDate));
-	var line = `${d.toISOString()},`;
-	Object.keys(data[extractionDate]).sort().forEach(replicaIndex => {
-		if(averageReplicaIndex){
-			//divide by number of replicas
-			line = line + (data[extractionDate][replicaIndex]/maxNOfReplicas).toFixed(2).toString()+","
-		}else{
-			line = line + (data[extractionDate][replicaIndex]/1).toFixed(2).toString()+","
-		}
-		
-	})//
-	fileWriter.write(`${line}\n`);
+    var d = new Date(parseInt(extractionDate));
+    var line = `${d.toISOString()},`;
+    Object.keys(data[extractionDate]).sort().forEach(replicaIndex => {
+        if (averageReplicaIndex) {
+            //divide by number of replicas
+            line = line + (data[extractionDate][replicaIndex] / maxNOfReplicas).toFixed(2).toString() + ","
+        } else {
+            line = line + (data[extractionDate][replicaIndex] / 1).toFixed(2).toString() + ","
+        }
+
+    }) //
+    fileWriter.write(`${line}\n`);
 })
+
+function getSince(index) {
+    var since = null;
+    if (index.accesses.since.$date === null || index.accesses.since.$date === undefined) {
+        if (index.accesses.since === null || index.accesses.since === undefined) {
+            console.log(`Can't read "since" from ${index}`);
+        }
+        //old versions generates without
+        since = new Date(`${index.accesses.since}`);
+    } else {
+
+        since = new Date(`${index.accesses.since.$date}`);
+    }
+    return since;
+}
